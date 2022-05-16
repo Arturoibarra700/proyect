e461b6ff9d7392581feec6f24d2c07dae5a3f63f
Carlos Briones <carlosbr-c@herbalife.com>
2022-05-16 09:48:24 -0500

    Adding swagger support

--- api/api.js
+++ api/api.js
@@ -5,6 +5,13 @@ const app = express();
 const v1 = require('./controllers/v1');
 const port = 5012;
 
+const swagger = require('./swagger.json');
+const ui = require('swagger-ui-express');
+
+// Using Swagger for documentation
+app.use('/swagger', ui.serve);
+app.use('/swagger', ui.setup(swagger));
+
 // JSON Middleware
 app.use(express.json());
 
@@ -13,8 +20,10 @@ const folder = path.resolve(__dirname, 'public');
 app.use('/', express.static(folder));
 
 // Routers
+// http://localhost:5012/v1
 app.use('/v1', v1);
 
+// http://localhost:5012
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
